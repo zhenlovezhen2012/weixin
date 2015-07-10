435203fb3ed5dd84583310fd77df4e15dccbc3f4
lijianxun <lijianxun@viathink.com>
2015-07-10 13:56:54 +0800

    添加事件推送处理;

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var xml2js = require('xml2js');
 var parseString = xml2js.parseString;
 var builder = new xml2js.Builder({rootName:"xml"});
 var global = require('../global');
+var msgRespService = require('../service/msgRespService');
 
 /* GET home page. */
 index.get('/', function(req, res, next) {
@@ -31,17 +32,26 @@ index.post('/',function(req,res){
         explicitRoot:false
     }, function (err, result) {
         console.log("收到消息: ",result);
-        //返回xml
-        var reMsg = {
-            ToUserName: result.FromUserName,
-            FromUserName: global.weixinAccount,
-            CreateTime: parseInt(Date.now()/1000),
-            MsgType: 'text',
-            Content: '您发送的信息是:'+result.Content
-        };
-        var xml = builder.buildObject(reMsg);
-        console.log('返回的xml: ',xml);
-        res.send(xml).end();
+        //根据MsgType判断消息类型
+        switch (result.MsgType){
+            case "text":
+                msgRespService.textResp(result,res);
+                break;
+            case "event":
+                msgRespService.eventResp(result,res);
+                break;
+            default :
+                var reMsg = {
+                    ToUserName: result.FromUserName,
+                    FromUserName: global.weixinAccount,
+                    CreateTime: parseInt(Date.now() / 1000),
+                    MsgType: "text",
+                    Content: "不支持事件类型: "+result.MsgType
+                };
+                var xml = builder.buildObject(reMsg);
+                console.log('返回的xml: ', xml);
+                res.send(xml).end();
+        }
     });
 });
 module.exports = index;
---
+++ service/msgRespService.js
@@ -0,0 +1,53 @@
+/**
+ * Created by jianxun on 2015/7/10.
+ */
+var xml2js = require('xml2js');
+var parseString = xml2js.parseString;
+var builder = new xml2js.Builder({rootName: "xml"});
+var global = require('../global');
+
+function textResp(messageObj, res) {
+    var reMsg = {
+        ToUserName: messageObj.FromUserName,
+        FromUserName: global.weixinAccount,
+        CreateTime: parseInt(Date.now() / 1000),
+        MsgType: 'text',
+        Content: '您发送的信息是:' + messageObj.Content
+    };
+    var xml = builder.buildObject(reMsg);
+    console.log('返回的xml: ', xml);
+    res.send(xml).end();
+}
+
+function eventResp(messageObj, res) {
+    switch (messageObj.Event){
+        case "subscribe":
+            var reMsg = {
+                ToUserName: messageObj.FromUserName,
+                FromUserName: global.weixinAccount,
+                CreateTime: parseInt(Date.now() / 1000),
+                MsgType: "text",
+                Content: "感谢您关注.."
+            };
+            var xml = builder.buildObject(reMsg);
+            console.log('返回的xml: ', xml);
+            res.send(xml).end();
+            break;
+        default :
+            var reMsg = {
+                ToUserName: messageObj.FromUserName,
+                FromUserName: global.weixinAccount,
+                CreateTime: parseInt(Date.now() / 1000),
+                MsgType: "text",
+                Content: "不支持事件类型: "+messageObj.Event
+            };
+            var xml = builder.buildObject(reMsg);
+            console.log('返回的xml: ', xml);
+            res.send(xml).end();
+            break;
+    }
+}
+module.exports = {
+    textResp: textResp,
+    eventResp:eventResp
+};
